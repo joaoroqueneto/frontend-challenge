1db00f58153964d70c11d1d4f6cfd2dcca2a4f12
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-06-23 11:39:33 -0300

    att

--- capputeeno/src/app/_components/header.tsx
+++ capputeeno/src/app/_components/header.tsx
@@ -1,6 +1,30 @@
 "use client"
+
+import {styled} from "styled-components"
+
+const Tagheader = styled.header`
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding: 20px 160px;
+`
+const Logo = styled.a`
+color: var(--globo-color);
+font-weight:400 ;
+font-size: 40px;
+line-height: 150%;
+`
+
 export function Header(){
     return(
-        <h1>ola mundo</h1>
+        <Tagheader>
+            <Logo>
+              teste teste 
+              
+            </Logo>
+            <div>
+
+            </div>
+        </Tagheader>
     )
 }
